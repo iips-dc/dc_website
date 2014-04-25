a384e69f27038b397020e0abb56f6315de040025
sachin-tanwar <sachintanwar69@gmail.com>
2014-04-25 16:56:58 +0530

    inserted logout.php and upload.php

---
+++ dc_website/grayscale/upload.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+$rollno=$_SESSION['rollno'];
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+/*&& ($_FILES["file"]["size"] < 200000)*/
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("images/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+	  $_FILES["file"]["name"]= $rollno . '.jpg';
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "images/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "images/" . $_FILES["file"]["tmp_name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+header("Location:front.php");
+?>
