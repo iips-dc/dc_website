d7082175b17c85c5b86270b28bd228871b77c6e0
sachin-tanwar <sachintanwar69@gmail.com>
2014-04-26 12:26:16 +0530

    developer page and contactus page

--- dc_website/footer.php
+++ dc_website/footer.php
@@ -5,11 +5,11 @@
     <footer>
             <div class="row" style="text-align:center;">
                 <div class="col-lg-3 col-md-3" >
-                    <p>Contact Us</p>
+                    <p><a href="contactus.php">Contact Us</a></p>
                    
             </div>
                 <div class="col-lg-3 col-md-3">
-                    <p>Site Developers</p>
+                    <p><a href="developer.php">Site Developers</a></p>
             </div>
                 <div class="col-lg-3 col-md-3">
                     <p>Help</p>
