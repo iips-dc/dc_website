9c608dfae880c6037e756198697ebc3f387a00c3
Akshay Verma <akshayverma295@gmail.com>
2014-09-18 00:43:55 +0530

    -a

--- aboutus.php
+++ aboutus.php
@@ -102,27 +102,7 @@
 
     <!-- /.container -->
 
-    <div class="container">
-
-        <hr>
-
-    <footer>
-            <div class="row" style="text-align:center;">
-                <div class="col-lg-3 col-md-3" >
-                    <p>Contact Us</p>
-                   
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Site Developers</p>
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Help</p>
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Copyright &copy; Company 2013</p>
-            </div>
-        </footer>
-    </div>
+     <?php include ('footer.html') ?>
     <!-- /.container -->
 
     <!-- JavaScript -->
--- open_source.php
+++ open_source.php
@@ -194,28 +194,7 @@
     </div>
 
 
-    <div class="container">
-
-        <hr>
-        <br><br>
-<footer>
-            <div class="row" style="text-align:center;">
-                <div class="col-lg-3 col-md-3" >
-                    <p>Contact Us</p>
-                   
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Site Developers</p>
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Help</p>
-            </div>
-                <div class="col-lg-3 col-md-3">
-                    <p>Copyright &copy; Company 2013</p>
-            </div>
-        </footer>
-
-    </div>
+   <?php include ('footer.html') ?>
     <!-- /.container -->
 
     <!-- JavaScript -->
