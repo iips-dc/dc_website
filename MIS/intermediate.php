ede8b290c6afb0a02414e5df9e999baa37fd7e63
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2014-10-08 17:52:02 +0530

    Knowledge

---
+++ MIS/intermediate.php
@@ -0,0 +1,24 @@
+ <?php
+//echo "string"; 
+
+ 
+include ('database_connect.php');
+$sql="SELECT * FROM `dc_members`";
+$result = mysqli_query($dbconnect,$sql) or die('ERROR'.mysqli_error($dbconnect));
+echo $email=$_GET['email'];
+echo "<br><br>";
+while($row = mysqli_fetch_array($result))
+  {
+    //echo $email;
+   
+  if($row['members_email'] == $email){
+    echo "string";
+    header("Location:http://localhost/gplus-quickstart-javascript/../dashboard/post.php");
+     break;
+  } 
+  
+  }
+  echo "You are not a authorised user!";
+
+
+?>
