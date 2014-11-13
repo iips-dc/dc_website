ac6a9c9838842af01a6073608dca6261381cc616
root <root@sachin-sony.(none)>
2014-11-13 17:04:14 +0530

    Updated project module

---
+++ MIS/project_new/project_taskbar.php
@@ -0,0 +1,17 @@
+<div class="col-lg-12">
+                <h1 class="page-header">Projects
+                    
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="add_project.php">Home</a>
+                    </li>
+                    <li><a href="add_review.php">Project Reviews</a>
+                    </li>
+                    <li><a href="add_documentation.php">Documentation</a>
+                    </li>
+                    
+                    
+                    
+                    
+                </ol>
+</div>
