30cf4312f67c24d8c65cd4907f7760888feddfb5
sachin-tanwar <sachintanwar69@gmail.com>
2015-03-20 15:13:39 +0530

    Recaptcha added to contactus page

--- contactus.php
+++ contactus.php
@@ -23,14 +23,28 @@
    <?php include ('nav.html');?> 
 
     <div class="container">
-    <br><br>
-    <h1> <b><u>CONTACT US</u></b> </h1><br><br>
-    <h2><b>pulkitvaishnav25@gmail.com</b></h2>
-    <h2><b>sachintanwar124@gmail.com</b></h2>
+        <br><br>
+        <h1> <b><u>CONTACT US</u></b> </h1><br>
+
+        <a href="http://www.google.com/recaptcha/mailhide/d?k=01znhN9NW9uZa__ZuSO1H3Rg==&amp;c=wgILhiGLwGV8FzP7MQ05wU7OhqQOAqgE2GMks-lGogE=" 
+        onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501znhN9NW9uZa__ZuSO1H3Rg\75\75\46c\75wgILhiGLwGV8FzP7MQ05wU7OhqQOAqgE2GMks-lGogE\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); 
+        return false;" title="Reveal this e-mail address"><h2>pul...</a>@gmail.com</h2>
+
+        <a href="http://www.google.com/recaptcha/mailhide/d?k=01DdpOH2bQrkwV3L3I0ng6Vw==&amp;c=zvaEN-U-SkmZBLjsBXuzTzuAKkrW-VKUBEX9GK0PXKM="
+         onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501DdpOH2bQrkwV3L3I0ng6Vw\75\75\46c\75zvaEN-U-SkmZBLjsBXuzTzuAKkrW-VKUBEX9GK0PXKM\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300');
+          return false;" title="Reveal this e-mail address"><h2>sac...</a>@gmail.com</h2>
+
+        <br><h2><b><u>ADDRESS :</u></b></h2>
+        <a href "http://www.iips.edu.in/"> <h3>International Institute of Professional Studies</h3></a>
+
+        <a href "http://www.dauniv.ac.in/"> <h3>Devi Ahilya University</h3></a>
+
+        <h3>Takshashila Campus, Khandwa Road Indore - 452001, INDIA</h3>
+
     
     </div>
    
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br>
 
     <?php include('footer.html');?>
 
